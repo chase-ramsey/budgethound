18930b36b8e40068f5d947ef35606fd2c2fb4cdb
Chase Ramsey <ramsey.chase@gmail.com>
2018-02-05 14:55:55 -0600

    Renamed project; created requirements.txt

--- budgethound/wsgi.py
+++ budgethound/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for honeypot project.
+WSGI config for budgethound project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "honeypot.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budgethound.settings")
 
 application = get_wsgi_application()
